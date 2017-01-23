04e93e1808114326c2deb2aa64e9758de4f7e083
douglas-raymond <douglas.raymond@hotmail.com>
2017-01-23 13:19:46 +0400

    Updates

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 var jsonUrl = "https://shopicruit.myshopify.com/admin/orders.json?page=1&access_token=c32313df0d0ef512ca64d5b336a0d7c6";
 var sum  = 0;
-$.getJSON(jsonUrl, function(data){
-	$.each(data.orders, function(idx, obj){
-				sum += parseFloat(obj.total_price);
-		}
-	});
-    alert("The sum of the total prices is: " + sum.toFixed(2));
+$.getJSON(jsonUrl, function(data)
+{
+	$.each(data.orders, function(idx, obj)
+	{
+	sum += parseFloat(variant.price);
+	};
+};
+    alert("The sum of the prices is: " + sum.toFixed(2));
 });
